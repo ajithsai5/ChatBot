733ce1872d4dcd860e059b5656f140351fce1bef
ajithsai <ajithsai123456789@gmail.com>
2025-03-22 10:33:22 +0530

    Adding more data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # main.py (entry point)
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from chatbot import chat_bp
 from document_processor import initialize_rag
+import os
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ initialize_rag('data/document.txt')
 
 # Register blueprint for chat routes
 app.register_blueprint(chat_bp)
+
+@app.route('/data-files')
+def list_data_files():
+    """List all files in the data folder"""
+    data_folder = 'data'
+    files = os.listdir(data_folder)
+    return jsonify(files)
 
 @app.route('/')
 def home():
